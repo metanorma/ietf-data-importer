b94181c0b5984b419631e0cdef0a52341d1b9043
Alexande B <abobrikovich@gmail.com>
2021-07-26 20:07:49 +0200

    #1 add simple json validation test

--- spec/metanorma/ietf/data_spec.rb
+++ spec/metanorma/ietf/data_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+require "json"
+
 RSpec.describe Metanorma::Ietf::Data do
   it "has a version number" do
     expect(Metanorma::Ietf::Data::VERSION).not_to be nil
   end
 
   it "workgroup exists and valid json" do
-    expect(false).to eq(true)
+    lib_path = File.join(File.dirname(__FILE__), "..", "..", "..", "lib")
+    wg_json = File.join(lib_path, "metanorma", "ietf", "data", "workgroup.json")
+    
+    expect { JSON.parse(File.read(wg_json)) }.not_to raise_error
   end
 end
